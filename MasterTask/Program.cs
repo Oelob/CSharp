3b32073dda24afb49a734465847891c78f84f428
Ivan <ab09@inbox.ru>
2022-12-04 23:47:53 +0500

    Added README

--- MasterTask/Program.cs
+++ MasterTask/Program.cs
@@ -10,28 +10,31 @@
 
             string[] OriginalMass(int length)
             {
-                string[] Mass = new string [length];
+                string[] Mass = new string[length];
                 int i = 0;
                 while (i < length)
                 {
                     Console.WriteLine("Введите строку массива ");
                     Mass[i] = Console.ReadLine();
                     i++;
-                } 
+                }
+                Console.WriteLine();
                 return Mass;
-              
+
             }
-            
+
+            // Создаем новый массив из элементов, удовлетворяющих условию задачи
+
             string[] GetNewMass(string[] array)
             {
                 string[] Mass = Array.Empty<string>();
                 int j = 0;
                 for (int i = 0; i < array.Length; i++)
                 {
-                    
+
                     if (array[i].Length < 4)
                     {
-                        Array.Resize(ref Mass, Mass.Length +1);
+                        Array.Resize(ref Mass, Mass.Length + 1);
                         Mass[j] = array[i];
                         j++;
                     }
@@ -39,15 +42,18 @@
                 }
                 return Mass;
             }
-            
+
+            // Ввод данных и вывод результатов
+
             Console.WriteLine("Введите размер массива: ");
             string[] FirstMass = OriginalMass(int.Parse(Console.ReadLine()));
-            Console.WriteLine($"[|{String.Join("| |", FirstMass)}|]");
+            Console.WriteLine($"Первоначальный массив: [|{String.Join("| |", FirstMass)}|]");
+            Console.WriteLine();
             string[] SortedMass = GetNewMass(FirstMass);
-            Console.WriteLine($"[|{String.Join("| |", SortedMass)}|]");
+            Console.WriteLine($"Итоговый массив: [|{String.Join("| |", SortedMass)}|]");
 
-                       
-           
+
+
         }
     }
 }
